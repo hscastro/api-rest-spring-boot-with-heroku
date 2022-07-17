02736ad9b82043da7542a2904b220f8edbd8716a
hscastro <halisonsc5@gmail.com>
2022-07-17 16:35:19 -0300

    Implemention method post - classe PersonController

--- src/main/java/com/hscastro/rest/resources/PersonController.java
+++ src/main/java/com/hscastro/rest/resources/PersonController.java
@@ -1,16 +1,36 @@
 package com.hscastro.rest.resources;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hscastro.rest.domain.Person;
+import com.hscastro.rest.services.PersonService;
 
 @RestController
 @RequestMapping("api/v1")
 public class PersonController {
+	
+	private PersonService personService;
+
+	public PersonController(PersonService personService) {
+		super();
+		this.personService = personService;
+	}
 
 	@GetMapping("/hello")
 	public ResponseEntity<String> getString(){
 		return ResponseEntity.ok("Olá mundo");
+	}
+	
+	@RequestMapping(value = "/persons", method = RequestMethod.POST,
+			produces = "application/json", consumes = "application/json")
+	public ResponseEntity<Person> create(@RequestBody Person person){
+		Person person_ = personService.save(person);
+		return ResponseEntity.status(HttpStatus.CREATED).body(person_);
 	}
 }
