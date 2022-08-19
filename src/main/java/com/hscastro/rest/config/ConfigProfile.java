a1566a367a29857b7f035a888928e390c4866708
hscastro <halisonsc5@gmail.com>
2022-08-18 23:20:34 -0300

    Mudanças no pom.xml

--- src/main/java/com/hscastro/rest/config/ConfigProfile.java
+++ src/main/java/com/hscastro/rest/config/ConfigProfile.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.hscastro.rest.services.DBService;
+	
 
 @Configuration
 @Profile("dev")
@@ -15,11 +16,7 @@ public class ConfigProfile {
 	@Autowired
 	private DBService dbTest;
 	
-<<<<<<< HEAD
-	@Value("${spring.jpa.generate-ddl}")
-=======
 	@Value("${spring.jpa.hibernate.ddl-auto}")
->>>>>>> main
 	private String ddl;
 	
 	@Bean
